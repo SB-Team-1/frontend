d359979ecf74002b113ed8524a38291e6510de96
Anthony-Rodriguez <7Anthony.Rodriguez7@gmail.com>
2021-03-24 12:03:48 -0400

    Add alliance routes
    - Added the skeleton of the routes to be used for alliances
    and their relevant files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import { Route, Switch } from "react-router";
 import "./App.css";
 import LandingPage from "./screens/LandingPage/LandingPage";
 import SignIn from "./screens/SignIn/SignIn";
+import BusinessIndex from "./screens/BusinessIndex"
+import BusinessProfile from "./screens/BusinessProfile"
+import AllianceIndex from "./screens/AllianceIndex"
+import AllianceProfile from "./screens/AllianceProfile"
+
 
 function App() {
   return (
@@ -15,6 +20,12 @@ function App() {
         </Route>
         <Route path="/business/:id">
           <BusinessProfile />
+        </Route>
+        <Route path="/alliances">
+          <AllianceIndex />
+        </Route>
+        <Route path="/alliances/:id">
+          <AllianceProfile />
         </Route>
       </Switch>
     </div>
