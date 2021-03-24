58e35f737b2cdb643295f5638a29728b90d807db
Evyn Newton <newton.evyn@gmail.com>
2021-03-24 11:22:55 -0400

    added some routes, changed api endpoint

--- src/services/apiconfig.js
+++ src/services/apiconfig.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const baseUrl =
-  process.env.NODE_ENV === "production"
-    ? "https://sballiance.herokuapp.com"
-    : "http://localhost:7165";
+const baseUrl = "https://sb-alliance-api.herokuapp.com";
 
 const api = axios.create({
   baseURL: baseUrl,
