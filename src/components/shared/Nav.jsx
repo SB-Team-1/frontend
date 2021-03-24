1e6facccec9bc01ec0cbe0b2a6511ab15beb90cc
Evyn Newton <newton.evyn@gmail.com>
2021-03-24 14:56:59 -0400

    added api calls in alliance container and some nav stuff

--- src/components/shared/Nav.jsx
+++ src/components/shared/Nav.jsx
@@ -1,10 +1,34 @@
-import { AppBar, Button } from "@material-ui/core";
+import { AppBar, Button, makeStyles, MenuItem } from "@material-ui/core";
 import React from "react";
+import { Link } from "react-router-dom";
 import Logo from "./Logo";
 
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    height: "7vh",
+    display: "flex",
+    flexFlow: "row wrap",
+    alignItems: "center",
+    justifyContent: "space-between",
+    color: theme.palette.primary.contrastText,
+  },
+  actionItems: {
+    display: "flex",
+    flexFlow: "row wrap",
+  },
+  link: {
+    color: theme.palette.primary.contrastText,
+    textDecoration: "none",
+  },
+}));
+
 export default function Nav({ currentUser, handleLogout }) {
+  const classes = useStyles();
+
+  const menuOptions = <></>;
+
   return (
-    <AppBar>
+    <AppBar className={classes.appBar}>
       <Logo />
       {currentUser ? (
         <>
@@ -12,6 +36,14 @@ export default function Nav({ currentUser, handleLogout }) {
           <Button onClick={handleLogout}>Logout</Button>
         </>
       ) : null}
+      <div className={classes.actionItems}>
+        <Link to="/alliances" className={classes.link}>
+          <MenuItem>Alliances</MenuItem>
+        </Link>
+        <Link to="/businesses" className={classes.link}>
+          <MenuItem>Local Businesses</MenuItem>
+        </Link>
+      </div>
     </AppBar>
   );
 }
