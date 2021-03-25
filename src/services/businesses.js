cbb141d7b6fd925275236f6c8de8f8407fde429e
Anthony-Rodriguez <7Anthony.Rodriguez7@gmail.com>
2021-03-24 21:43:06 -0400

    Change end points

--- src/services/businesses.js
+++ src/services/businesses.js
@@ -2,31 +2,31 @@ import api from "./apiconfig";
 
 //Create a new business
 export const createBusiness = async (data) => {
-  const resp = await api.post("/business-create/", { business: data });
+  const resp = await api.post("/businesses", { business: data });
   return resp.data;
 };
 
 //Edit a business
 export const updateBusiness = async (id, data) => {
-  const resp = await api.patch(`/business-edit/${id}/`, { business: data });
+  const resp = await api.patch(`/businesses/${id}`, { business: data });
   return resp.data;
 };
 
 //Delete a business
 export const deleteBusiness = async (id) => {
-  const resp = await api.delete(`/business/${id}/`);
+  const resp = await api.delete(`/businesses/${id}`);
   return resp;
 };
 
 //Get one business
 export const getBusiness = async (id) => {
-  const resp = await api.get(`/business/${id}`);
+  const resp = await api.get(`/businesses/${id}`);
   return resp.data;
 };
 
 //show all businesses
 export const showBusinesses = async () => {
-  const resp = await api.get("/business");
+  const resp = await api.get("/businesses");
   return resp.data;
 };
 
