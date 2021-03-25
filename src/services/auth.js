b9fed522a3ed60c9bcae451d271107cab3d6566e
Evyn Newton <newton.evyn@gmail.com>
2021-03-24 21:43:08 -0400

    changed endpoints

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,10 +1,10 @@
 import api from "./apiconfig";
 
 export const loginUser = async (loginData) => {
-  const resp = await api.post("/sign-in/", { credentials: loginData });
+  const resp = await api.post("/auth/login", { user: loginData });
   console.log(resp);
   localStorage.setItem("authToken", resp.data.user.token);
-  api.defaults.headers.common.authorization = `Token ${resp.data.user.token}`;
+  api.defaults.headers.common.authorization = `Bearer ${resp.data.user.token}`;
   return resp.data.user;
 };
 
@@ -13,22 +13,22 @@ export const registerUser = async (registerData) => {
     user: registerData,
   });
   localStorage.setItem("authToken", resp.data.token);
-  api.defaults.headers.common.authorization = `Token ${resp.data.token}`;
+  api.defaults.headers.common.authorization = `Bearer ${resp.data.token}`;
   return resp.data.user;
 };
 
-export const changePassword = async ({ oldPassword, newPassword }) => {
-  const resp = await api.put("/change-pw/", {
-    passwords: { new: newPassword, old: oldPassword },
-  });
-  return resp.data;
-};
+// export const changePassword = async ({ oldPassword, newPassword }) => {
+//   const resp = await api.put("/change-pw/", {
+//     passwords: { new: newPassword, old: oldPassword },
+//   });
+//   return resp.data;
+// };
 
 export const verifyUser = async () => {
   const token = localStorage.getItem("authToken");
   if (token) {
-    api.defaults.headers.common.authorization = `Token ${token}`;
-    const resp = await api.get("/verify/");
+    api.defaults.headers.common.authorization = `Bearer ${token}`;
+    const resp = await api.get("/auth/verify/");
     return resp.data;
   }
   return null;
