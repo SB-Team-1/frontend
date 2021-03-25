8df09d93aeb6dd4afbfcfc1c528824dc62abb417
Anthony-Rodriguez <7Anthony.Rodriguez7@gmail.com>
2021-03-25 15:56:16 -0400

    Make a landing page

--- src/components/shared/LandingStock.jsx
+++ src/components/shared/LandingStock.jsx
@@ -1,22 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import landingstock from "./images/landingstock.jpeg";
 
 export default function LandingStock() {
   return (
-    <Link to="/">
-      <div style={{}}>
+      <div>
         <img
           src={landingstock}
           alt="local market"
           style={{
-            paddingTop: "7vh",
             height: "35vh",
             width: "120%",
             objectFit: "cover"
           }}
         />
       </div>
-    </Link>
   );
 }
