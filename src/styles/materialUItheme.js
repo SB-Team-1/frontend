7c45f524c1ab2d6d5d0799f06d0ad433aa194625
Evyn Newton <newton.evyn@gmail.com>
2021-03-25 10:01:30 -0400

    updated theme

--- src/styles/materialUItheme.js
+++ src/styles/materialUItheme.js
@@ -2,17 +2,26 @@ import { createMuiTheme } from "@material-ui/core/styles";
 
 export const theme = createMuiTheme({
   palette: {
-    // primary: {
-    //   // light: "#672b44",
-    //   // main: "#3a001d",
-    //   // dark: "#3a001d",
-    //   // contrastText: "#ffffff",
-    // },
-    // secondary: {
-    //   // light: "#757373",
-    //   main: "",
-    //   // dark: "#494848",
-    //   // contrastText: "#ffffff",
-    // },
+    primary: {
+      main: "#F5F4F6",
+    },
+    secondary: {
+      main: "#822840",
+    },
+  },
+  typography: {
+    fontFamily: [
+      "Lato",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
   },
 });
